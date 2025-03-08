fbb1896705ed6a269baf8bf443a7862f2aecbd84
Kedirhussen-atri <hussen@atrihub.io>
2025-03-07 17:39:25 -0800

    update package build

--- tools/build.R
+++ tools/build.R
@@ -7,71 +7,48 @@ library(knitr)
 
 ## Set working directory ----
 ## To set the project directory as working directory
-## current_script_dir <- dirname(rstudioapi::getSourceEditorContext()$path)
-## current_script_dir <- str_remove(current_script_dir, "/tools")
-## setwd(current_script_dir)
+## cur_script_dir <- dirname(rstudioapi::getSourceEditorContext()$path)
+## cur_script_dir <- str_remove(string = cur_script_dir, pattern = "/tools$")
+## setwd(cur_script_dir)
 
 ## Data preparation ----
+# Data downloaded date
+DATA_DOWNLOADED_DATE <- "2025-02-25"
 callr::rscript(
   script = "./data-raw/data_prep.R",
-  wd = "."
+  wd = ".",
+  cmdargs = list(DATA_DOWNLOADED_DATE = DATA_DOWNLOADED_DATE)
 )
 
-RECODE_VALUE <- TRUE
+DECODE_VALUE <- TRUE
 
-if (RECODE_VALUE) {
+if (DECODE_VALUE) {
+  # A boolean indicator to use the updated data dictionary.
+  # Only applicable if the UPDATED_DATADIC is created.
+  # Please see line 385 in the `./data-raw/data_prep.R`.
+  USE_UPDATED_DATADIC <- TRUE
   callr::rscript(
     script = "./data-raw/data_prep_recode.R",
-    wd = "."
+    wd = ".",
+    cmdargs = list(USE_UPDATED_DATADIC = USE_UPDATED_DATADIC)
   )
+} else {
+  USE_UPDATED_DATADIC <- FALSE
 }
 
-UPDATE_DERIVED_DATASET <- TRUE
+## Generate derived/analysis dataset ----
+INCLUDE_DERIVED_DATASET <- TRUE
 
-if (UPDATE_DERIVED_DATASET) {
-  # Generate derived dataset and stored in `./data` directory -----
-  devtools::load_all("./")
-  temporary_file <- tempfile()
-  vignette_file_path <- file.path(".", "vignettes", "ADNIMERGE2-Derived-Data.Rmd")
-  knitr::purl(input = vignette_file_path, output = temporary_file)
-  source(file = temporary_file)
-  DERIVED_DATASET_LIST <- c("DM", "AE", "QS")
-
-  # To apply use_data() function: use_data_modified()
-  source(file = file.path(".", "tools", "data-prepare-utils.R"))
-  lapply(DERIVED_DATASET_LIST, function(tbl_name) {
-    assign("dd", get(tbl_name))
-    # using use_data function
-    use_data_modified(
-      dataset_name = tbl_name,
-      dataset = dd,
-      edit_type = "create",
-      run_script = TRUE
-    )
-    rm(list = c("dd", "tbl_name"))
-  })
-
-  # Derived data dictionary
-  derived_data_dic <- ls()[str_detect(string = ls(), pattern = "_data_dic")]
-  derived_data_dic_patterns <- str_c("^",
-    str_to_lower(DERIVED_DATASET_LIST),
-    "_",
-    collapse = "|"
+if (INCLUDE_DERIVED_DATASET) {
+  # List of derived dataset
+  DERIVED_DATASET_LIST <- c(
+    "DM", "AE", "QS", "SC", "RS", "NV", "BS","VS",
+    "ADSL", "ADAE", "ADQS", "METACORES"
   )
-  derived_data_dic <- derived_data_dic[str_detect(
-    string = derived_data_dic,
-    pattern = derived_data_dic_patterns
-  )]
-
-  DERIVED_DATADIC <- mget(derived_data_dic) %>%
-    bind_rows()
-
-  derived_datadic_dir_path <- file.path(".", "data-raw", "derived-datadic")
-  unlink(derived_datadic_dir_path, recursive = TRUE)
-  dir.create(derived_datadic_dir_path)
-  save(
-    list = "DERIVED_DATADIC",
-    file = file.path(derived_datadic_dir_path, "DERIVED_DATADIC.rda")
+  callr::rscript(
+    script = "./tools/generate_derived_data.R",
+    wd = ".",
+    cmdargs = list(DERIVED_DATASET_LIST = DERIVED_DATASET_LIST)
   )
 } else {
   DERIVED_DATASET_LIST <- NULL
@@ -81,21 +58,27 @@ if (UPDATE_DERIVED_DATASET) {
 callr::rscript(
   script = "./tools/document.R",
   wd = ".",
-  cmdargs = list(DERIVED_DATASET_LIST = DERIVED_DATASET_LIST)
+  cmdargs = list(
+    DERIVED_DATASET_LIST = DERIVED_DATASET_LIST,
+    USE_UPDATED_DATADIC = USE_UPDATED_DATADIC
+  )
 )
 
-## Finalized package building ----
+## Finalize package building ----
 devtools::load_all("./")
 devtools::document()
-devtools::check(error_on = "error", vignettes = UPDATE_DERIVED_DATASET)
-pkg_dir <- devtools::build(vignettes = UPDATE_DERIVED_DATASET)
+devtools::check(error_on = "error", vignettes = INCLUDE_DERIVED_DATASET)
+pkg_dir <- devtools::build(vignettes = INCLUDE_DERIVED_DATASET)
+install.packages(pkg_dir)
 
 # Build README.md ----
-build_readme()
+devtools::build_readme()
 
 # Build website ----
 # run once:
-# usethis::use_pkgdown()
+# For non manual website development:
+# # usethis::use_pkgdown() # overwrite any existed `_pkgdown.yml` file
+# For manual website development: required to modify the pre-existed `_pkgdown.yml` as needed
 # pkgdown::build_site()
 # publish online:
-# pkgdown::deploy_to_branch()
+# # pkgdown::deploy_to_branch()
