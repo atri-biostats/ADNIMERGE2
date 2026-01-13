0e637ce055a758b3cb395d44ac61da7a2f7cff32
Kedirhussen-atri <hussen@atrihub.io>
2026-01-12 16:54:06 -0800

    skip for non-exist files

--- tools/remove-files.R
+++ tools/remove-files.R
@@ -9,21 +9,47 @@ check_arg(args, 3)
 INPUT_DIR <- args[1]
 OUTPUT_DIR <- args[2]
 PATTERN <- args[3]
-
-# Copy files to `output` directory ----
-file_action(
-  input_dir = INPUT_DIR,
-  output_dir = OUTPUT_DIR,
-  file_extension = PATTERN,
-  action = "copy", 
-  show_message = FALSE
+soure_file_list <- list.files(
+  path = INPUT_DIR,
+  pattern = PATTERN,
+  full.names = TRUE,
+  recursive = FALSE
 )
-
-# Remove files from previous directory ------
-file_action(
-  input_dir = INPUT_DIR,
-  output_dir = OUTPUT_DIR,
-  file_extension = PATTERN,
-  action = "remove", 
-  show_message = TRUE
-)
+SOURCE_FILE_STATUS <- any(!is.na(soure_file_list))
+if (SOURCE_FILE_STATUS == TRUE) {
+  # Copy files to `output` directory ----
+  file_action(
+    input_dir = INPUT_DIR,
+    output_dir = OUTPUT_DIR,
+    file_extension = PATTERN,
+    action = "copy",
+    show_message = TRUE
+  )
+  # Remove files from previous directory ------
+  file_action(
+    input_dir = INPUT_DIR,
+    output_dir = OUTPUT_DIR,
+    file_extension = PATTERN,
+    action = "remove",
+    show_message = TRUE
+  )
+  soure_file_list <- soure_file_list[!is.na(soure_file_list)]
+  cli::cli_inform(
+    c(
+      "i" = paste0(
+        "{.val {basename(soure_file_list)}} file{?s} {?is/are} transferred",
+        " from {.path {INPUT_DIR}} to {.path {OUTPUT_DIR}}"
+      )
+    )
+  )
+} else {
+  cli::cli_inform(
+    c(
+      "i" = "No file with {.val {PATTERN}} pattern exists in {.path {INPUT_DIR}}. \n",
+      "!" = paste0(
+        "No file {.path {PATTERN}} with pattern is transferred from ",
+        "{.path {INPUT_DIR}} to {.path {OUTPUT_DIR}}."
+      )
+    )
+  )
+}
