d80087a89a5a1e9fc10294a8eb4dc856571d7607
Kedirhussen-atri <hussen@atrihub.io>
2026-01-15 13:38:40 -0800

    update vignettes-yaml and #19

--- tools/vignettes-yaml.R
+++ tools/vignettes-yaml.R
@@ -1,28 +1,23 @@
-# Changing the default params of INCLUDE_PACC in vignettes -----
-## This allows changing a rmarkdown yaml using a script rather a manual update
+# Modify the default param YAML of `INCLUDE_PACC` in vignettes -----
+## This allows to modify the default/current rmarkdown param yaml using script rather than manual update
 
-# Vignettes files ----
-vignette_dir <- file.path(".", "vignettes")
-rmd_file_list <- list.files(
-  path = vignette_dir,
-  pattern = "\\.Rmd$",
-  full.names = TRUE,
-  all.files = TRUE,
-  recursive = FALSE
+## Util-functions ----
+source(file.path(".", "tools", "yaml-utils.R"))
+source(file.path(".", "tools", "data-prepare-utils.R"))
+source(file.path(".", "tools", "prepare-datadict.R"))
+
+## Input args ----
+arg_list <- commandArgs(trailingOnly = TRUE)
+check_arg(arg_list, 3)
+INPUT_DIR <- arg_list[1]
+CURRENT_PACC_PARAM <- arg_list[2]
+NEW_PACC_PARAM <- arg_list[3]
+
+## Modify param YAML -----
+modify_rmd_param_yaml(
+  dir_path = INPUT_DIR,
+  file_path = NULL,
+  current_param = CURRENT_PACC_PARAM,
+  new_param = NEW_PACC_PARAM, 
+  value_as_logical = TRUE
 )
-
-# Change the default yaml values of params$INCLUDE_PACC
-default_params <- " INCLUDE_PACC: true| INCLUDE_PACC: TRUE"
-update_params <- " INCLUDE_PACC: false"
-
-for (i in seq_along(rmd_file_list)) {
-  status <- xfun::gsub_file(rmd_file_list[i], default_params, update_params, fixed = TRUE)
-  if (!is.null(status)) {
-    cli::cli_alert_info(text = c(
-      "Default params in {.path {rmd_file_list[i]}} is changed \n",
-      "{.field {default_params}} -> {.field {update_params}}"
-    ))
-  } else {
-    cli::cli_alert_info(text = "{.path {rmd_file_list[i]}} is not changed!")
-  }
-}
